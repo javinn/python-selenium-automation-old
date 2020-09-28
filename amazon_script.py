76fc947c0af0ba39b344dad739392365fa0c20dd
javinn <javid.mehdizade@gmail.com>
2020-09-28 18:44:39 -0400

    Modified amazon_script.py

--- amazon_script.py
+++ amazon_script.py
@@ -1,3 +1,25 @@
 from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Crome(executable_path = )
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+driver.implicitly_wait(4)
+
+driver.get('https://www.amazon.com')
+
+sleep(2)
+
+input_field = driver.find_element(By.ID, 'twotabsearchtextbox')
+
+search_icon = input_field.send_keys('Desk')
+sleep(2)
+
+search_icon = driver.find_element(By.XPATH, "//span[@id='nav-search-submit-text']/input")
+
+search_icon.click()
+
+search_word = driver.find_element(By.XPATH, "//span[@dir='auto' and @class='a-color-state a-text-bold']")
+assert search_word.text == '"Desk"', f'Error. Searching word should be "Desk", not {search_word.text}'
